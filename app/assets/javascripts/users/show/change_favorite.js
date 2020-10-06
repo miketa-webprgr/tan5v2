e121e89d884f9fcb94f97e10e34ca9c4964aee66
itsuki-n22 <kazenokaori22@gmail.com>
2020-10-06 23:09:36 +0900

    modify icon view on user/show : remove icons border and change bg-color

--- app/assets/javascripts/users/show/change_favorite.js
+++ app/assets/javascripts/users/show/change_favorite.js
@@ -1,6 +1,6 @@
 jQuery.changeFavorite = function(){
   $(document).on('click','[id*="favorite-no-"]',function(){
-    let starHtml = $(this);
+    let starHtml = $(this).find("button");
     let starClass = starHtml.attr('class')
     if(starClass == undefined || !starClass.match(/favorited/)  ){
       starHtml.addClass('favorited')
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,18 @@ class FavoritesController < Base
   before_action :current_user
 
   def change
-    @favorite = @current_user.favorite.find_by(wordnote_id: params[:wordnote_id])
-    @wordnote_id = params[:wordnote_id]
+    @favorite = @current_user.favorite.find_by(wordnote_id: favorite_params[:wordnote_id])
+    @wordnote_id = favorite_params[:wordnote_id]
     if @favorite
       @favorite.destroy
-      render action: "destroy" if @current_user.id.to_s == params[:user_id]
+      render action: "destroy" if @current_user.id.to_s == favorite_params[:user_id]
     else
-      @current_user.favorite.build(wordnote_id: params[:wordnote_id]).save
+      @current_user.favorite.build(wordnote_id: favorite_params[:wordnote_id]).save
     end
   end
+
+  private 
+    def favorite_params
+      params.require(:favorite).permit(:user_id, :wordnote_id)
+    end
 end
